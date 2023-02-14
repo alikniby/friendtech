0a5010216a262b039583fdc3f96a017a41cb07cf
Ali kniby <33119604+alikniby@users.noreply.github.com>
2023-02-14 15:11:37 +0100

    try2
    code change

--- friendbot.py
+++ friendbot.py
@@ -3,7 +3,7 @@ from random import choice
 from flask import Flask,request
 import os
 import openai
-
+  
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion
